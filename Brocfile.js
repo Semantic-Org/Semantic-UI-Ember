0d0369175c0993741a3881b2606ecb3a379f5417
Nathan Palmer <nathan@sprucemedia.com>
2014-12-10 17:00:39 -0500

    Added Semantic UI 1.2.0

--- Brocfile.js
+++ Brocfile.js
@@ -18,4 +18,7 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/semantic-ui/dist/semantic.css');
+app.import('bower_components/semantic-ui/dist/semantic.js');
+
 module.exports = app.toTree();
