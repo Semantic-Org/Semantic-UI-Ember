db700cd6c5ed99dec44e6bc3737deb1a4047640a
Joshua Forisha <jforisha@softlayer.com>
2014-12-18 09:57:03 -0600

    Import semantic-ui for parent app

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'semantic-ui-ember'
+  name: 'semantic-ui-ember',
+
+  included: function (app) {
+    app.import({
+      development: 'bower_components/semantic-ui/dist/semantic.css',
+      production: 'bower_components/semantic-ui/dist/semantic.min.css'
+    });
+
+    app.import({
+      development: 'bower_components/semantic-ui/dist/semantic.js',
+      production: 'bower_components/semantic-ui/dist/semantic.min.js'
+    });
+  }
 };
