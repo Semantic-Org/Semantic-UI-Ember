f24a5c1ebbe6052e83bf3db839cbb7a0473aea2b
Nathan Palmer <nathan@nathanpalmer.com>
2015-02-03 00:30:42 -0500

    Changed how we're dealing with the ui-dropdown-item so we bind and unbind on the didInsertElement and willDestroyElement events Removed the data-value since complex objects aren't getting set through this

--- addon/components/ui-dropdown-item.js
+++ addon/components/ui-dropdown-item.js
@@ -3,32 +3,23 @@ import Ember from 'ember';
 export default Ember.SelectOption.extend({
   tagName: 'div',
   classNames: 'item',
-  attributeBindings: [ 'data_value:data-value' ],
 
-  data_value: function() {
-    var valuePath = this.get('parentView.optionValuePath');
-    return this.get(valuePath);
-  }.property('parentView.optionValuePath'),
-
-  initialized: false,
-  initialize: function() {
-    var _this = this;
-    Ember.run(function() {
-      Ember.run.scheduleOnce('afterRender', _this, _this.set_value);
-    });
-  }.on('init'),
-
-  set_value: function() {
+  bindData: function() {
     var valuePath = this.get('parentView.optionValuePath');
 
     if (!valuePath) {
       return;
     }
+
     if (this.$() == null) {
       return;
     }
 
     this.$().data('value', this.get(valuePath));
     this.set('initialized',true);
-  }
+  }.on('didInsertElement'),
+
+  unbindData: function() {
+    this.$().removeData('value')
+  }.on('willDestroyElement')
 });
