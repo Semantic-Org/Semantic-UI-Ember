7e71ac8becbbd61ee69effdf3129123043f3588e
Nathan Palmer <nathan@nathanpalmer.com>
2015-02-03 11:12:49 -0500

    The first time the onChange is called the value is undefined. I tracked this down to a line of code within semantic that calls set selected with no value. The line is referenced and instead we're pulling the value directly out of data.value since we get access to the element

--- addon/components/ui-dropdown.js
+++ addon/components/ui-dropdown.js
@@ -20,6 +20,15 @@ export default Ember.Select.extend(Base, DataAttributes, {
       this.execute('set selected', value);
     }
   }.on('didInsertElement'),
+
+  onChange: function(value, text, $element) {
+    if (value === undefined) {
+      // The initial set selected doesn't have an value. This is potentially a problem
+      // within the main Semantic library
+      //
+      // https://github.com/Semantic-Org/Semantic-UI/blob/master/src/definitions/modules/dropdown.js#L85
+      value = $element.data('value');
+    }
     return this.set('value', value);
   },
 
