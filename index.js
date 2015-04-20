6c3dd78770f53b562bddd6373b99f6b6ddb385d6
Florian Günther <mail@gee-f.de>
2015-04-20 10:13:11 +0200

    introduce options to skip CSS import

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ module.exports = {
   name: 'semantic-ui-ember',
 
   included: function (app) {
-    app.import({
-      development: 'bower_components/semantic-ui/dist/semantic.css',
-      production: 'bower_components/semantic-ui/dist/semantic.min.css'
-    });
+    var options = (app && app.options.semanticUIOptions) || {};
+
+    if(!options['skipCSS']){
+      app.import({
+        development: 'bower_components/semantic-ui/dist/semantic.css',
+        production: 'bower_components/semantic-ui/dist/semantic.min.css'
+      });
+    };
 
     app.import({
       development: 'bower_components/semantic-ui/dist/semantic.js',
