54ec3511596e7153998c0eea43b9098e081cd354
Nathan Palmer <nathan@nathanpalmer.com>
2015-08-24 07:59:16 -0400

    Added tests for ui-nag

--- addon/components/ui-nag.js
+++ addon/components/ui-nag.js
@@ -4,6 +4,7 @@ import Base from '../mixins/base';
 export default Ember.Component.extend(Base, {
   module: 'nag',
   classNames: [ 'ui', 'nag' ],
+  attributeBindings: [ 'storageMethod', 'key', 'expires', 'domain', 'path', 'value' ],
 
   init: function() {
     this._super();
