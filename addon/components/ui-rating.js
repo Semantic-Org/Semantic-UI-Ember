3a503517dde5660183bd752bb918729829f6ab89
Nathan Palmer <nathan@nathanpalmer.com>
2015-08-24 07:59:40 -0400

    Added tests for ui-rating

--- addon/components/ui-rating.js
+++ addon/components/ui-rating.js
@@ -3,7 +3,19 @@ import Base from '../mixins/base';
 
 var Rating = Ember.Component.extend(Base,{
   module: 'rating',
-  classNames: ['ui', 'rating']
+  classNames: ['ui', 'rating'],
+
+  init: function() {
+    this._super();
+
+    if (!this.get('initialRating') && this.get('rating')) {
+      this.set('initialRating', this.get('rating'));
+    }
+  },
+
+  updateRating: Ember.observer('rating', function() {
+    this.execute('set rating', this.get('rating'));
+  })
 });
 
 export default Rating;
