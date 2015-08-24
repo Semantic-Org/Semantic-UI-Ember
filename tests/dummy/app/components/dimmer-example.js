2460970b6697afa944a9d7338d162c8616a67227
Nathan Palmer <nathan@nathanpalmer.com>
2015-08-23 21:50:09 -0400

    Removed the unused component parameter for dimmer-example

--- tests/dummy/app/components/dimmer-example.js
+++ tests/dummy/app/components/dimmer-example.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    show: function(component) {
+    show: function() {
       this.$('.ui.segment').dimmer('show');
     },
 
-    hide: function(component) {
+    hide: function() {
       this.$('.ui.segment').dimmer('hide');
     }
   }
