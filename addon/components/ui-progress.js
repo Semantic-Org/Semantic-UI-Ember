05e8c15a9f08fe9ffd002ba4cdf70520640fbd8a
Nathan Palmer <nathan@nathanpalmer.com>
2015-08-24 07:59:26 -0400

    Added tests for ui-progress

--- addon/components/ui-progress.js
+++ addon/components/ui-progress.js
@@ -3,7 +3,12 @@ import Base from '../mixins/base';
 
 var Progress = Ember.Component.extend(Base,{
   module: 'progress',
-  classNames: ['ui', 'progress']
+  classNames: ['ui', 'progress'],
+  attributeBindings: [ 'percent', 'total', 'value' ],
+
+  updatePercent: Ember.observer('percent', function() {
+    this.execute('set percent', this.get('percent'));
+  })
 });
 
 export default Progress;
---
+++ tests/integration/components/ui-progress-test.js
@@ -0,0 +1,45 @@
+/* global stop, start */
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('ui-dropdown', 'Integration | Component | ui progress', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  this.render(hbs`
+    {{#ui-progress percent=40 class="teal indicating"}}
+      <div class="bar"></div>
+      <div class="label">Completed</div>
+    {{/ui-progress}}
+  `);
+
+  assert.equal(this.$('.ui.progress').length, 1);
+});
+
+test('binding updates progress', function(assert) {
+  assert.expect(4);
+
+  var _this = this;
+
+  this.set('progress', 40);
+  this.render(hbs`
+    {{#ui-progress percent=progress class="teal indicating"}}
+      <div class="bar"></div>
+      <div class="label">Completed</div>
+    {{/ui-progress}}
+  `);
+
+  assert.equal(this.$('.ui.progress').length, 1);
+  assert.equal(this.$('.ui.progress').attr('data-percent'), 40);
+  var width = this.$('.ui.progress .bar').css('width');
+  this.set('progress', 60);
+
+  stop();
+  setTimeout(function() {
+    start();
+
+    assert.equal(_this.$('.ui.progress').attr('data-percent'), 60);
+    assert.notEqual(_this.$('.ui.progress .bar').css('width'), width);
+  }, 500);
+});
