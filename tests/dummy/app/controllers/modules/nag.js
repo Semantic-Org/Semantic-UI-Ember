817473cda6ce038d0e483fd1bf01a22723066417
Nathan Palmer <nathan@nathanpalmer.com>
2015-08-23 22:57:40 -0400

    Removed the unnecessary variable for nag

--- tests/dummy/app/controllers/modules/nag.js
+++ tests/dummy/app/controllers/modules/nag.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend(Ember.Evented, {
   actions: {
-    clear: function(name) {
+    clear: function() {
       $('.cookie.nag').nag('clear');
       $('.cookie.nag').nag('show');
     }
