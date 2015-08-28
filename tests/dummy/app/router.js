6ed8bc045d280140bd38fec9c6736a3be19c7ec5
Nathan Palmer <nathan@nathanpalmer.com>
2015-08-28 01:06:25 -0400

    Upgraded to Ember 2.0!

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('modules', function() {
+  this.route('modules', function() {
     this.route('accordion');
     this.route('checkbox');
     this.route('dimmer');
