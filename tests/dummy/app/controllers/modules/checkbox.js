54711353bc284e44a70adbe174d9b65939cdbd97
gowthaman.chandrasekar <gowthaman.chandrasekar@zohocorp.com>
2015-11-16 12:09:02 +0530

    Update - Dummy app for checkbox.

--- tests/dummy/app/controllers/modules/checkbox.js
+++ tests/dummy/app/controllers/modules/checkbox.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   checked: false,
-
+  
+  fruit: "apple",
+    
   actions: {
     set_checked: function(value) {
       this._super();
