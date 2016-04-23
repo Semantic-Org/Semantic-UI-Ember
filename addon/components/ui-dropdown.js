34935ad9c76561d2a603b60bb11a1b7470a10141
Aaron Hansen <aaronbhansen@gmail.com>
2016-04-23 00:02:23 -0600

    Removing value binding on dropdown

--- addon/components/ui-dropdown.js
+++ addon/components/ui-dropdown.js
@@ -5,33 +5,20 @@ import DataAttributes from '../mixins/data-attributes';
 export default Ember.Component.extend(Base, DataAttributes, {
   module: 'dropdown',
   classNames: [ 'ui', 'dropdown' ],
-  tagName: 'div',
 
   didInsertElement() {
     this._super(...arguments);
     var selected = this.get('selected');
     if (typeof selected !== "undefined" && selected !== null) {
-      this.set('boundValue', 'selected');
       this.execute('set selected', this.getSelected(selected));
     }
-
-    var value = this.get('value');
-    if (typeof value !== "undefined" && value !== null) {
-      Ember.deprecate('Bind to selected on ui-dropdown instead of value as semantic doesn\'t update the display when the value is set', false);
-      this.set('boundValue', 'value');
-      this.execute('set value', this.getValue(value));
-    }
   },
-  
+
   getSelected(selected) {
     return selected;
   },
-  
-  getValue(value) {
-    return value;
-  },
 
   _onChange: function(value/*, text, $element*/) {
-    this.set(this.get('boundValue') || 'selected', value);
+    this.set('selected', value);
   }
 });
