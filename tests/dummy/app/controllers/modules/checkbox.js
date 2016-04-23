5c7aeb7b9cbf3c6d6141f75f9500ce3279167617
Aaron Hansen <aaronbhansen@gmail.com>
2016-04-23 15:06:09 -0600

    Adding better checkbox / radio examples

--- tests/dummy/app/controllers/modules/checkbox.js
+++ tests/dummy/app/controllers/modules/checkbox.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   checked: false,
+  pre_checked: true,
+  _action_checked: false,
+  action_checked: Ember.computed('_action_checked', function() {
+    return this.get('_action_checked');
+  }),
+
+  fruit: null,
+  color: "red",
 
   actions: {
     set_checked: function(value) {
-      this._super();
-      console.log(value);
+      this.set('_action_checked', value);
     }
   }
 });
