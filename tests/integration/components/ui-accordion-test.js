78dfe210349a747ff14f88987074afb9574ebbca
Aaron Hansen <aaronbhansen@gmail.com>
2016-04-23 22:45:07 -0600

    Adding additional accordion tests

--- tests/integration/components/ui-accordion-test.js
+++ tests/integration/components/ui-accordion-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
@@ -8,20 +9,91 @@ moduleForComponent('ui-accordion', 'Integration | Component | ui accordion', {
 test('it renders', function(assert) {
   this.render(hbs`
     {{#ui-accordion class="styled"}}
-      <div class="title">
+      <div class="title" data-id="title-1">
         Semantic UI
       </div>
-      <div class="content">
+      <div class="content" data-id="content-1">
         Accordion Component
       </div>
-      <div class="title">
+      <div class="title" data-id="title-2">
         Section Two
       </div>
-      <div class="content">
+      <div class="content" data-id="content-2">
         Content Two
       </div>
     {{/ui-accordion}}
   `);
 
-  assert.equal($('.ui.accordion').length, 1);
+  // Test default state
+  assert.equal(this.$('.ui.accordion').length, 1);
+  assert.equal(this.$('.ui.accordion .active').length, 0);
+});
+
+test('clicking activates title', function(assert) {
+  this.render(hbs`
+    {{#ui-accordion class="styled"}}
+      <div class="title" data-id="title-1">
+        Semantic UI
+      </div>
+      <div class="content" data-id="content-1">
+        Accordion Component
+      </div>
+      <div class="title" data-id="title-2">
+        Section Two
+      </div>
+      <div class="content" data-id="content-2">
+        Content Two
+      </div>
+    {{/ui-accordion}}
+  `);
+
+  // Test clicking activates accordion
+  this.$('.ui.accordion [data-id=title-2]').click();
+  assert.equal(this.$('.ui.accordion [data-id=title-2].active').length, 1);
+  assert.equal(this.$('.ui.accordion .active').length, 1);
+});
+
+test('dynamically added content is clickable', function(assert) {
+  this.set('panes', Ember.A([]));
+
+  this.render(hbs`
+    {{#ui-accordion class="styled"}}
+      <div class="title" data-id="title-1">
+        Semantic UI
+      </div>
+      <div class="content" data-id="content-1">
+        Accordion Component
+      </div>
+      <div class="title" data-id="title-2">
+        Section Two
+      </div>
+      <div class="content" data-id="content-2">
+        Content Two
+      </div>
+      {{#each panes as |pane|}}
+      <div class="title" data-id="extra-title-{{pane}}">
+        Extra Section {{pane}}
+      </div>
+      <div class="content" data-id="extra-content-{{pane}}">
+        Extra Content {{pane}}
+      </div>
+      {{/each}}
+    {{/ui-accordion}}
+  `);
+
+  assert.equal(this.$('.ui.accordion').length, 1);
+  assert.equal(this.$('.ui.accordion .title').length, 2);
+  assert.equal(this.$('.ui.accordion .content').length, 2);
+
+  Ember.run(() => {
+    this.get('panes').pushObjects([1,2]);
+  });
+
+  assert.equal(this.$('.ui.accordion .title').length, 4);
+  assert.equal(this.$('.ui.accordion .content').length, 4);
+
+  // Test clicking activates accordion
+  this.$('.ui.accordion [data-id=extra-title-1]').click();
+  assert.equal(this.$('.ui.accordion [data-id=extra-title-1].active').length, 1);
+  assert.equal(this.$('.ui.accordion .active').length, 1);
 });
