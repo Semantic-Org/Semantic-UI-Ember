73a4a4783ec7415281089ea939bfc596bc492f62
Aaron Hansen <aaronbhansen@gmail.com>
2016-05-09 23:10:59 -0600

    Start of removing bindings and using attrs

--- addon/mixins/base.js
+++ addon/mixins/base.js
@@ -2,16 +2,18 @@ import Ember from 'ember';
 import Semantic from '../semantic';
 
 // Static properties to ignore
-const DEBUG = ['debug', 'performance', 'verbose'];
-const STANDARD = ['name', 'namespace', 'className', 'metadata', 'selector'];
-const EMBER = ['context', 'on', 'template', 'execute'];
+// const DEBUG = ['debug', 'performance', 'verbose'];
+// const STANDARD = ['name', 'namespace', 'className', 'metadata', 'selector'];
+// const EMBER = ['context', 'on', 'template', 'execute'];
+
+const EMBER = Ember.A(['class', 'classNames', 'classNameBindings']);
 
 Semantic.BaseMixin = Ember.Mixin.create({
   init() {
     this._super(...arguments);
 
-    if (!this.get('module')) {
-      return Ember.Logger.error('Module was not declared on semantic extended type');
+    if (Ember.isBlank(this.get('module'))) {
+      return Ember.Logger.error('A module was not declared on semantic extended type');
     }
   },
 
@@ -29,28 +31,22 @@ Semantic.BaseMixin = Ember.Mixin.create({
       verbose: Semantic.UI_VERBOSE
     };
 
-    for (key in component.settings) {
-      prop = component.settings[key];
-      if (window.$.inArray(key, DEBUG) >= 0) {
-        continue;
-      }
+    for (let key in this.attrs) {
+      let value = this.attrs[key];
 
-      if (window.$.inArray(key, STANDARD) >= 0) {
-        continue;
-      }
-
-      if (typeof prop === 'function' && typeof (this.get(key) || this.get(`_${key}`)) !== 'function') {
-        continue;
-      }
-
-      if (window.$.inArray(key, EMBER) >= 0) {
-        value = this.get(`ui_${key}`);
-      } else {
-        if (typeof this.get(key) !== 'undefined') {
-          value = this.get(key);
-        } else {
-          value = this.get(`_${key}`);
+      // ensure it isn't a mutable object
+      if (typeof value === "object") {
+        let objectKeys = Ember.A(Object.keys(value));
+        if (objectKeys.any((objectkey) => objectkey.startsWith('MUTABLE_CELL') )) {
+          value = value.value;
         }
+      }
+
+      if (Ember.isBlank(component.settings[key])) {
+        if (!EMBER.contains(key)) {
+          Ember.Logger.debug(`You passed in the property '${key}', but a setting doesn't exist on the Semantic UI module: ${module}`);
+        }
+        continue;
       }
 
       if (value != null) {
@@ -62,29 +58,60 @@ Semantic.BaseMixin = Ember.Mixin.create({
       }
     }
 
+    // for (key in component.settings) {
+    //   prop = component.settings[key];
+    //   if (window.$.inArray(key, DEBUG) >= 0) {
+
+    //   if (window.$.inArray(key, STANDARD) >= 0) {
+    //     continue;
+    //   }
+
+    //   if (typeof prop === 'function' && typeof (this.get(key) || this.get(`_${key}`)) !== 'function') {
+    //     continue;
+    //   }
+
+    //   if (window.$.inArray(key, EMBER) >= 0) {
+    //     value = this.get(`ui_${key}`);
+    //   } else {
+    //     if (typeof this.get(key) !== 'undefined') {
+    //       value = this.get(key);
+    //     } else {
+    //       value = this.get(`_${key}`);
+    //     }
+    //   }
+
+    //   if (value != null) {
+    //     if (typeof value === 'function') {
+    //       custom[key] = Ember.run.bind(this, this.updateFunctionWithParameters(key, value));
+    //     } else {
+    //       custom[key] = value;
+    //     }
+    //   }
+    // }
+
     return custom;
   },
 
-  updateProperty(property) {
-    return function() {
-      this.execute('set ' + property, this.get(property));
-    };
-  },
+  // updateProperty(property) {
+  //   return function() {
+  //     this.execute('set ' + property, this.get(property));
+  //   };
+  // },
 
   updateFunctionWithParameters(key, fn) {
     return function() {
       var args = [].splice.call(arguments, 0);
-      var internal = this.get(`_${key}`);
+      // var internal = this.get(`_${key}`);
 
-      if (internal) {
-        internal.apply(this, args);
-      }
+      // if (internal) {
+      //   internal.apply(this, args);
+      // }
 
-      if (internal !== fn) {
-        return fn.apply(this, [this].concat(args));
-      }
+      // if (internal !== fn) {
+      return fn.apply(this, [this].concat(args));
+      // }
 
-      return true;
+      // return true;
     };
   },
 
@@ -96,24 +123,24 @@ Semantic.BaseMixin = Ember.Mixin.create({
     this._super(...arguments);
     this.initializeModule();
 
-    var _this = this;
-    var properties = {};
+    // var _this = this;
+    // var properties = {};
 
     // Modules without setable properties
-    properties = this.execute('internal', 'set');
-    var property;
+    // properties = this.execute('internal', 'set');
+    // var property;
 
-    if (typeof properties === "object" && !Ember.isArray(properties)) {
-      for(property in properties) {
-        if (!properties.hasOwnProperty(property)) {
-          continue;
-        }
+    // if (typeof properties === "object" && !Ember.isArray(properties)) {
+    //   for(property in properties) {
+    //     if (!properties.hasOwnProperty(property)) {
+    //       continue;
+    //     }
 
-        if (this.hasOwnProperty(property)) {
-          _this.addObserver(property, _this, _this.updateProperty(property));
-        }
-      }
-    }
+    //     if (this.hasOwnProperty(property)) {
+    //       _this.addObserver(property, _this, _this.updateProperty(property));
+    //     }
+    //   }
+    // }
   },
 
   willDestroyElement() {
