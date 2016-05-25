a719dd8db062d1b18168859432759b6d32da8b7b
Aaron Hansen <aaronbhansen@gmail.com>
2016-05-25 15:57:04 -0600

    Adding dimmer unit tests

--- tests/integration/components/ui-dimmer-test.js
+++ tests/integration/components/ui-dimmer-test.js
@@ -6,19 +6,100 @@ moduleForComponent('ui-dimmer', 'Integration | Component | ui dimmer', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{ui-dimmer}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
+  assert.expect(1);
   // Template block usage:
   this.render(hbs`
-    {{#ui-dimmer}}
+    {{#ui-dimmer class="ui segment"}}
       template block text
     {{/ui-dimmer}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('.ui.segment .ui.dimmer').length, 1);
+});
+
+test('dimmer shows and hides on click', function(assert) {
+  assert.expect(3);
+  // Template block usage:
+  this.render(hbs`
+    {{#ui-dimmer class="ui segment" on="click" duration=(hash show=0 hide=0)}}
+      template block text
+    {{/ui-dimmer}}
+  `);
+
+  assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 0);
+  this.$('.ui.segment').click();
+
+  stop();
+  setTimeout(() => {
+    start();
+    assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 1);
+    this.$('.ui.segment').click();
+
+    stop();
+    setTimeout(() => {
+      start();
+      assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 0);
+    }, 100);
+  }, 100);
+});
+
+test('dimmer only works on scoped element for shows and hides on click', function(assert) {
+  assert.expect(3);
+  // Template block usage:
+  this.render(hbs`
+    {{#ui-dimmer on="click" onElement=".ui.segment" duration=(hash show=0 hide=0)}}
+      <div class="ui segment">
+        template block text
+      </div>
+    {{/ui-dimmer}}
+  `);
+
+  assert.equal(this.$().children('.ui.dimmer').length, 0);
+  this.$().click();
+
+  stop();
+  setTimeout(() => {
+    start();
+    // Check all dimmers
+    assert.equal(this.$('.ui.dimmer.active').length, 0);
+    this.$('.ui.segment').click();
+
+    stop();
+    setTimeout(() => {
+      start();
+      // animation takes a second after click
+      assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 1);
+    }, 100);
+  }, 100);
+});
+
+test('dimmer shows and hides from composable action', function(assert) {
+  assert.expect(3);
+  // Template block usage:
+  this.render(hbs`
+    {{#ui-dimmer on="click" onElement=".ui.segment" duration=(hash show=0 hide=0) as |execute|}}
+      <div class="ui button" {{action execute "show"}} data-id="show">Show</div>
+      <div class="ui button" {{action execute "hide"}} data-id="hide">Hide</div>
+      <div class="ui segment">
+        template block text
+      </div>
+    {{/ui-dimmer}}
+  `);
+
+  assert.equal(this.$().children('.ui.dimmer').length, 0);
+  this.$('[data-id=show]').click();
+
+  stop();
+  setTimeout(() => {
+    start();
+    assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 1);
+    this.$('[data-id=hide]').click();
+
+    stop();
+    setTimeout(() => {
+      start();
+      // animation takes a second after click
+      assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 0);
+    }, 100);
+  }, 100);
 });
