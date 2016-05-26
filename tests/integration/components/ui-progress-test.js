2ec013a3258f3dfd77c79172063df63aab82bd9e
Aaron Hansen <aaronbhansen@gmail.com>
2016-05-26 10:53:06 -0600

    Adding additional progress tests

--- tests/integration/components/ui-progress-test.js
+++ tests/integration/components/ui-progress-test.js
@@ -6,8 +6,8 @@ moduleForComponent('ui-progress', 'Integration | Component | ui progress', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(1);
+test('it renders with percent', function(assert) {
+  assert.expect(2);
 
   this.render(hbs`
     {{#ui-progress percent=40 class="teal indicating"}}
@@ -17,9 +17,24 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$('.ui.progress').length, 1);
+  assert.equal(this.$('.ui.progress').attr('data-percent'), 40)
 });
 
-test('binding updates value progress', function(assert) {
+test('it renders with value', function(assert) {
+  assert.expect(2);
+
+  this.render(hbs`
+    {{#ui-progress value=40 class="teal indicating"}}
+      <div class="bar"></div>
+      <div class="label">Completed</div>
+    {{/ui-progress}}
+  `);
+
+  assert.equal(this.$('.ui.progress').length, 1);
+  assert.equal(this.$('.ui.progress').attr('data-percent'), 40)
+});
+
+test('binding updates precent progress', function(assert) {
   assert.expect(4);
 
   this.set('progress', 40);
@@ -44,12 +59,62 @@ test('binding updates value progress', function(assert) {
   }, 500);
 });
 
+test('binding updates precent progress with total', function(assert) {
+  assert.expect(4);
+
+  this.set('progress', 40);
+  this.render(hbs`
+    {{#ui-progress percent=progress total=30 class="teal indicating"}}
+      <div class="bar"></div>
+      <div class="label">Completed</div>
+    {{/ui-progress}}
+  `);
+
+  assert.equal(this.$('.ui.progress').length, 1);
+  assert.equal(this.$('.ui.progress').attr('data-percent'), 40);
+  var width = this.$('.ui.progress .bar').css('width');
+  this.set('progress', 60);
+
+  stop();
+  setTimeout(() => {
+    start();
+
+    assert.equal(this.$('.ui.progress').attr('data-percent'), 60);
+    assert.notEqual(this.$('.ui.progress .bar').css('width'), width);
+  }, 500);
+});
+
 test('binding updates progress', function(assert) {
+  assert.expect(4);
+
+  this.set('value', 50);
+  this.render(hbs`
+    {{#ui-progress value=value progress=value class="teal indicating"}}
+      <div class="bar"></div>
+      <div class="label">Completed</div>
+    {{/ui-progress}}
+  `);
+
+  assert.equal(this.$('.ui.progress').length, 1);
+  assert.equal(this.$('.ui.progress').attr('data-percent'), 50);
+  var width = this.$('.ui.progress .bar').css('width');
+  this.set('value', 70);
+
+  stop();
+  setTimeout(() => {
+    start();
+
+    assert.equal(this.$('.ui.progress').attr('data-percent'), 70);
+    assert.notEqual(this.$('.ui.progress .bar').css('width'), width);
+  }, 500);
+});
+
+test('binding updates progress with total', function(assert) {
   assert.expect(4);
 
   this.set('value', 15);
   this.render(hbs`
-    {{#ui-progress value=(unbound value) progress=value total=30 class="teal indicating"}}
+    {{#ui-progress value=value progress=value total=30 class="teal indicating"}}
       <div class="bar"></div>
       <div class="label">Completed</div>
     {{/ui-progress}}
