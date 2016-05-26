fd8ec36208df2bde6e074f7e69072d79290f8705
Aaron Hansen <aaronbhansen@gmail.com>
2016-05-26 10:35:04 -0600

    Updating rating

--- addon/components/ui-rating.js
+++ addon/components/ui-rating.js
@@ -3,28 +3,15 @@ import Base from '../mixins/base';
 
 var Rating = Ember.Component.extend(Base, {
   module: 'rating',
-  classNames: [ 'ui', 'rating' ],
+  classNames: ['ui', 'rating'],
+  ignorableAttrs: ['rating'],
 
-  // init() {
-  //   this._super(...arguments);
-  //   if (!this.attrs.initialRating && this.attrs.rating) {
-  //     this.attrs.initialRating = this.attrs.rating;
-  //   }
-  // },
-
-  // didInsertElement() {
-  //   this.set('initialized', false);
-  //   this._super(...arguments);
-  //   this.set('initialized', true);
-  // },
-
-  // _onRate(value) {
-  //   // Values are set on init, and causes render errors
-  //   if (!this.get('initialized')) {
-  //     return;
-  //   }
-  //   this.set('rating', value);
-  // }
+  willInitSemantic(settings) {
+    this._super(...arguments);
+    if (!settings.initialRating && this._getAttrValue('rating')) {
+      settings.initialRating = this._getAttrValue('rating');
+    }
+  }
 });
 
 export default Rating;
