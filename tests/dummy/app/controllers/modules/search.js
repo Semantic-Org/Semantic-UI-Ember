49607389c314901dc0f1556eee607344557c7a07
Aaron Hansen <aaronbhansen@gmail.com>
2016-05-29 17:05:46 -0600

    Adding local search demo and tests

---
+++ tests/dummy/app/controllers/modules/search.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  commonPasswords: [
+    { title: "password" },
+    { title: "123456" },
+    { title: "12345678" },
+    { title: "1234" },
+    { title: "qwerty" },
+    { title: "12345" },
+    { title: "dragon" },
+    { title: "heart" },
+    { title: "baseball" },
+    { title: "football" },
+    { title: "letmein" },
+    { title: "monkey" },
+    { title: "696969" },
+    { title: "abc123" },
+    { title: "mustang" },
+    { title: "michael" },
+    { title: "shadow" },
+    { title: "master" },
+    { title: "jennifer" },
+    { title: "111111" }
+  ]
+});
--- tests/integration/components/ui-search-test.js
+++ tests/integration/components/ui-search-test.js
@@ -17,3 +17,44 @@ test('it renders', function(assert) {
 
   assert.equal(this.$('.ui.search').length, 1);
 });
+
+test('searching content works', function(assert) {
+  assert.expect(5);
+
+  this.set('commonPasswords', [
+    { title: "bobby" },
+    { title: "12345" }
+  ]);
+
+  this.set('query', null);
+  this.set('selected', null);
+
+  this.render(hbs`
+    {{#ui-search source=commonPasswords onSearchQuery=(action (mut query)) onSelect=(action (mut selected))}}
+      <input class="prompt" type="text" placeholder="Common passwords...">
+      <div class="results"></div>
+    {{/ui-search}}
+  `);
+
+  assert.equal(this.$('.ui.search').length, 1);
+  assert.equal(this.get('query'), null);
+  assert.equal(this.get('selected'), null);
+
+  this.$('input').focus();
+  this.$('input').val('123');
+  this.$('.ui.search').search('query');
+
+  assert.equal(this.get('query'), "123");
+
+  this.$('.ui.search').search('show results');
+
+  let done = assert.async();
+
+  setTimeout(() => {
+    this.$('.result').addClass('active');
+    this.$('input').trigger(window.jQuery.Event('keydown', { which: 13 }));
+
+    assert.equal(this.get('selected.title'), "12345");
+    done();
+  }, 500);
+});
