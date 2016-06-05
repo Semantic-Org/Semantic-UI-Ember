695cd7d57295ecff9b36a48245e358465544c710
Aaron Hansen <aaronbhansen@gmail.com>
2016-06-04 22:47:41 -0600

    Adding dropdown multiple select support

--- addon/components/ui-dropdown.js
+++ addon/components/ui-dropdown.js
@@ -3,7 +3,7 @@ import Base from '../mixins/base';
 
 const _proxyCallback = function(callbackName) {
   return function(value, text, $element) {
-    this.attrs[callbackName](this._getObjectOrValue(value), text, $element, this);
+    return this.attrs[callbackName](this._getObjectOrValue(value), text, $element, this);
   };
 };
 
@@ -39,35 +39,103 @@ export default Ember.Component.extend(Base, {
 
   didInitSemantic() {
     this._super(...arguments);
-    var selected = this.get('selected');
     this._swapAttrs('selected');
 
+    var selected = this.get('selected');
     if (Ember.isPresent(selected)) {
-      let selectedValue = this._getObjectKeyByValue(selected);
-      this.execute('set selected', selectedValue);
+      this.setSemanticAttr('selected', selected);
     }
   },
 
   getSemanticAttr(attrName) {
     if (attrName === 'selected') {
-      let value = this.execute('get value');
-      return this._getObjectOrValue(value);
+      if (this.execute('is multiple')) {
+        let values = this.execute('get values');
+        let returnValues = [];
+        for (let value of values) {
+          returnValues.push(this._getObjectOrValue(value));
+        }
+        return returnValues;
+      } else {
+        let value = this.execute('get value');
+        return this._getObjectOrValue(value);
+      }
     }
     return this._super(...arguments);
   },
 
   setSemanticAttr(attrName, attrValue) {
     if (attrName === 'selected') {
-      let value = this._getObjectKeyByValue(attrValue);
+
+      let value;
+      if (Ember.isArray(attrValue)) {
+
+        if (!this.execute('is multiple')) {
+          Ember.Logger.error("Selected is an array of values, but the dropdown doesn't have the class 'multiple'");
+          return;
+        }
+
+        value = [];
+        for (let item of attrValue) {
+          value.push(this._getObjectKeyByValue(item));
+        }
+      } else {
+        value = this._getObjectKeyByValue(attrValue);
+      }
+
       if (Ember.isBlank(value)) {
         return this.execute('clear');
       }
+
       return this.execute('set selected', value);
     }
     return this._super(...arguments);
   },
 
-  _onChange: _proxyCallback('onChange'),
+  areAttrValuesEqual(attrName, attrValue, moduleValue) {
+    if (this.execute('is multiple')) {
+      // If attr passed in is an array, we are assuming that its the collection getting updated and that
+      // all module values must equal the attrValues
+      if (Ember.isArray(attrValue)) {
+        // Loop through the collections and see if they are equal
+        for (let value of attrValue) {
+          let equal = false;
+          for (let module of moduleValue) {
+            if (this._super(attrName, value, module)) {
+              equal = true;
+              break;
+            }
+          }
+          if (!equal) {
+            return false;
+          }
+        }
+      } else if (Ember.isArray(moduleValue)) { // otherwise, just try to see one of the values in the module equals the attr value
+        for (let item of moduleValue) {
+          if (this._super(attrName, attrValue, item)) {
+            return true; // We found a match
+          }
+        }
+        return false;
+      }
+    }
+    // If its not multiple then default to base
+    return this._super(...arguments);
+  },
+
+  _onChange(value, text, $element) {
+    let returnValue;
+    if (this.execute('is multiple')) {
+      let values = this.execute('get values');
+      returnValue = [];
+      for (let item of values) {
+        returnValue.push(this._getObjectOrValue(item));
+      }
+    } else {
+      returnValue = this._getObjectOrValue(value);
+    }
+    return this.attrs.onChange(returnValue, text, $element, this);
+  },
   _onAdd: _proxyCallback('onAdd'),
   _onRemove: _proxyCallback('onRemove'),
 
