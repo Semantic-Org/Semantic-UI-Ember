653722c4bedd1820d85e84768d7f7e0047e2d357
Aaron Hansen <aaronbhansen@gmail.com>
2016-07-15 23:07:58 -0600

    Adding promise aware map value

--- addon/helpers/map-value.js
+++ addon/helpers/map-value.js
@@ -1,7 +1,46 @@
 import Ember from 'ember';
+import { isPromise } from '../utils/promise-tools';
+import { isFulfilled } from '../utils/promise-tools';
+import { getPromiseContent } from '../utils/promise-tools';
 
-export function mapValue([action, ...params]) {
-  return action(...params);
-}
+// Code referenced from https://github.com/fivetanley/ember-promise-helpers
+export default Ember.Helper.extend({
+  compute([action, maybePromise]) {
+    if (!isPromise(maybePromise)) {
+      return action(maybePromise);
+    }
+    // If we've already fulfilled, just return to avoid returning null
+    // Probably could tie into SetValue, something to think about later
+    if (isFulfilled(maybePromise)) {
+      return action(getPromiseContent(maybePromise));
+    }
 
-export default Ember.Helper.helper(mapValue);
+    // If the type wasn't a PromiseProxy or RSVP, check if we resolved for .then
+    if (this._wasSettled && maybePromise === this._promise) {
+      return action(this._value);
+    }
+
+    this.ensureLatestPromise(maybePromise, (promise) => {
+      promise.then((value) => {
+        if (maybePromise === this._promise) {
+          this._wasSettled = true;
+          this._value = value;
+          // This will recompue the value and fire the _wasSettled check above
+          this.recompute();
+        }
+      }).catch((error) => {
+        Ember.Logger.error('Promise died in map-value');
+        Ember.Logger.error(error);
+      });
+    });
+    return null;
+  },
+
+  ensureLatestPromise(promise, callback) {
+    // It's a new promise, reset
+    this._wasSettled = false;
+    this._promise = promise;
+
+    callback.call(this, Ember.Promise.resolve(promise));
+  }
+});
