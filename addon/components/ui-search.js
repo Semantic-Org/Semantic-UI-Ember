d093c11749fffe94421ba6b2067c4af07e36b391
Aurelio Saraiva <aureliosaraiva@gmail.com>
2017-08-02 11:52:28 -0300

    suggestion solve bug ui-source

--- addon/components/ui-search.js
+++ addon/components/ui-search.js
@@ -3,5 +3,24 @@ import Base from '../mixins/base';
 
 export default Ember.Component.extend(Base, {
   module: 'search',
-  classNames: ['ui', 'search']
+  classNames: ['ui', 'search'],
+
+  didInitSemantic(...args) {
+    this._super(...args);
+    this.get('_bindableAttrs').addObject('source');
+  },
+  execute(...args){
+    const cmd = args[0];
+    const attrValue = args[1];
+
+    if(cmd === 'set source') {
+      let module = this.getSemanticModule();
+      if (module) {
+        return module.apply(this.getSemanticScope(), [{
+          source: attrValue
+        }]);
+      }
+    }
+    return this._super(...args);
+  }
 });
