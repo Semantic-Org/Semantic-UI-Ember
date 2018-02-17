0bc2f7f73b8cb0ab2bfa85ec5c0cc0dd15e57c04
Arnav Gupta <arnav@codingblocks.com>
2018-02-17 14:57:05 +0530

    update test config
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,104 +2,34 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      },
+      name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': '2.16'
         }
       }
     },
     {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
+      name: 'ember-lts-2.18',
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': '2.18'
         }
       }
     },
     {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
+      name: 'ember-latest',
       npm: {
         devDependencies: {
-          'ember-source': null
+          'ember-source': 'latest'
         }
       }
     },
     {
       name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-default',
-      npm: {
-        devDependencies: {}
-      }
-    },
-    {
-      name: 'semantic-2.2.4',
-      bower: {
-        dependencies: {
-          'semantic-ui': '2.2.4'
-        }
-      }
-    },
-    {
-      name: 'semantic-latest',
-      bower: {
-        dependencies: {
-          'semantic-ui': 'latest'
+          'ember-source': 'beta'
         }
       }
     }
