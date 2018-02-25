1137d4e59b743008af1b726b4a31026c6471b2bc
Arnav Gupta <arnav@codingblocks.com>
2018-02-26 01:55:32 +0530

    convert to es6 code
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 'use strict';
 
-var path = require('path');
+const path = require('path')
 
-var defaults = {
+const defaults = {
   import: {
     css: true,
     javascript: true,
@@ -20,13 +20,13 @@ var defaults = {
     images: 'assets/themes/default/assets/images',
     fonts: 'assets/themes/default/assets/fonts'
   }
-};
+}
 
-var getDefault = require('./lib/utils/get-default');
+const getDefault = require('./lib/utils/get-default')
 
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
-var map = require('broccoli-stew').map;
+const Funnel = require('broccoli-funnel')
+const mergeTrees = require('broccoli-merge-trees')
+const map = require('broccoli-stew').map
 
 module.exports = {
   options: {
@@ -40,18 +40,18 @@ module.exports = {
   name: 'semantic-ui-ember',
 
   included: function (app) {
-    var options = (app && app.project.config(app.env)['SemanticUI']) || {};
+    const options = (app && app.project.config(app.env)['SemanticUI']) || {}
 
-    var importCss = getDefault('import', 'css', [options, defaults]);
+    const importCss = getDefault('import', 'css', [options, defaults])
     if (importCss) {
-      var sourceCss = getDefault('source', 'css', [options, defaults]);
+      const sourceCss = getDefault('source', 'css', [options, defaults])
       app.import({
         development: path.join(sourceCss, 'semantic.css'),
         production: path.join(sourceCss, 'semantic.min.css')
       });
     }
 
-    var importJavascript = getDefault('import', 'javascript', [options, defaults]);
+    const importJavascript = getDefault('import', 'javascript', [options, defaults])
     if (importJavascript) {
       this.sourceJavascript = getDefault('source', 'javascript', [options, defaults]);
       app.import({
@@ -60,37 +60,37 @@ module.exports = {
       });
     }
 
-    var importImages = getDefault('import', 'images', [options, defaults]);
+    const importImages = getDefault('import', 'images', [options, defaults])
     if (importImages) {
-      var sourceImage = getDefault('source', 'images', [options, defaults]);
-      var imageOptions = { destDir: getDefault('destination', 'images', [options, defaults]) };
+      const sourceImage = getDefault('source', 'images', [options, defaults])
+      const imageOptions = {destDir: getDefault('destination', 'images', [options, defaults])}
       app.import(path.join(sourceImage, 'flags.png'), imageOptions);
     }
 
-    var importFonts = getDefault('import', 'fonts', [options, defaults]);
+    const importFonts = getDefault('import', 'fonts', [options, defaults])
     if (importFonts) {
-      var fontExtensions = ['.eot','.otf','.svg','.ttf','.woff','.woff2'];
-      var sourceFont = getDefault('source', 'fonts', [options, defaults]);
-      var fontOptions = { destDir: getDefault('destination', 'fonts', [options, defaults]) };
-      for (var i = fontExtensions.length - 1; i >= 0; i--) {
+      const fontExtensions = ['.eot', '.otf', '.svg', '.ttf', '.woff', '.woff2']
+      const sourceFont = getDefault('source', 'fonts', [options, defaults])
+      const fontOptions = {destDir: getDefault('destination', 'fonts', [options, defaults])}
+      for (let i = fontExtensions.length - 1; i >= 0; i--) {
         app.import(path.join(sourceFont, 'icons' + fontExtensions[i]), fontOptions);
       }
     }
   },
 
   treeForVendor: function(vendorTree) {
-    var trees = [];
+    const trees = []
 
     if (vendorTree) {
       trees.push(vendorTree);
     }
 
-    var sourceJavascript = this.sourceJavascript;
+    const sourceJavascript = this.sourceJavascript
     if (sourceJavascript) {
-      var semanticJsTree =  new Funnel(sourceJavascript, {
+      let semanticJsTree = new Funnel(sourceJavascript, {
         srcDir: '/',
         files: ['semantic.js', 'semantic.min.js']
-      });
+      })
 
       semanticJsTree = map(semanticJsTree,
           (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
