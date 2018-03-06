3d5fc60119527a01a72a1650c364769f345b61cf
Arnav Gupta <arnav@codingblocks.com>
2018-03-06 07:59:25 +0530

    make nodejs 6 compatible
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- blueprints/semantic-ui-custom/index.js
+++ blueprints/semantic-ui-custom/index.js
@@ -1,23 +1,35 @@
 /* eslint-env node */
-const config = require ('../../config/environment')()
-const EOL = require('os').EOL;
+const config = require('../../config/environment')()
+const EOL = require('os').EOL
 
 module.exports = {
   description: 'Installs user-customisable version of semantic-ui',
 
-  normalizeEntityName() {},
-  async afterInstall(options) {
-    this.ui.write('Removing semantic-ui-css')
-    await this.removePackageFromProject('semantic-ui-css')
-    this.ui.write('Adding semantic-ui custom folder to .gitignore')
-    await this.insertIntoFile('.gitignore',
-      `
+  normalizeEntityName() {
+  },
+  afterInstall(options) {
+    return this.ui.write('Removing semantic-ui-css').then(function () {
+
+      return this.removePackageFromProject('semantic-ui-css')
+
+    }).then(function () {
+
+      this.ui.write('Adding semantic-ui custom folder to .gitignore')
+
+      return this.insertIntoFile('.gitignore',
+        `
 /semantic/*
 !/semantic/src/site/*
 !/semantic/src/theme.config
         `
-    )
-    this.ui.write('Installing semantic-ui')
-    await this.addPackageToProject('semantic-ui', config.SEMANTIC_UI_VERSION)
+      )
+
+    }).then(function () {
+
+      this.ui.write('Installing semantic-ui')
+      return this.addPackageToProject('semantic-ui', config.SEMANTIC_UI_VERSION)
+
+    })
+
   }
-};
+}
