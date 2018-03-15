23b16d5803eceb02f6693e914f799c04b093a66e
Julien Palmas <julien.palmas@gmail.com>
2018-03-15 15:42:49 +0100

    move component templates in the addon

--- addon/components/ui-dimmer.js
+++ addon/components/ui-dimmer.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import Base from '../mixins/base';
+import layout from '../templates/components/ui-dimmer';
 
 export default Ember.Component.extend(Base, {
+  layout,
   module: 'dimmer'
 });
