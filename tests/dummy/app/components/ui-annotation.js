add1a3ffd007f781cf331279fcc48fc101613194
Arnav Gupta <arnav@codingblocks.com>
2018-03-16 03:11:32 +0530

    usage details updated
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- tests/dummy/app/components/ui-annotation.js
+++ tests/dummy/app/components/ui-annotation.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   type: "handlebars",
-
   classNames: ['annotation', 'transition'],
   classNameBindings: ['showing:visible:hidden']
 });
